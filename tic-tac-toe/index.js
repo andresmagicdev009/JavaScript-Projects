df77e8f83cdc85945ca3e7264e669b8e43912855
andresmagicdev009 <andres.rodriguezso09@gmail.com>
2026-07-09 22:17:53 -0500

    Tres en raya wip

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -68,3 +68,10 @@ const createPlayer = (name, symbol) => {
     return { name, symbol };
 }
 
+// Definir las combinaciones ganadoras 
+
+const combinacionesGanadoras = [
+    [0,1,2], [3,4,5], [6,7,8],
+    [0,3,6], [1,4,7], [2,5,8],
+    [0,4,8], [2,4,6]
+]
